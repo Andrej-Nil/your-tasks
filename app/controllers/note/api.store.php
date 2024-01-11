372be31e7ffde493394a13a4c49b47df3159ac93
Andrej-Nil <and.kucheroff@yandex.ru>
2024-01-11 09:00:33 +0500

    Раскоментировал добавление заметки в бд

--- app/controllers/note/api.store.php
+++ app/controllers/note/api.store.php
@@ -6,12 +6,12 @@ $auth = \classes\App::get(\classes\Auth::class);
 $user = $auth->get();
 $fillable = ['text', 'color'];
 $data = load($fillable);
-if(!array_key_exists('color', $data)){
+if (!array_key_exists('color', $data)) {
     $data['color'] = 'white';
 }
 
 $validation = $validator->validate($data, [
-    'text'=>[
+    'text' => [
         'required' => true,
         'max' => 191,
     ],
@@ -20,43 +20,39 @@ $validation = $validator->validate($data, [
     ]
 ]);
 
-if(!$validation->hasErrors()){
+if (!$validation->hasErrors()) {
     $values = [
         $data['text'],
         getColor($data['color']),
         $user['id']
     ];
 
-$data['id'] = 123;
+    $data['id'] = 123;
 
-    echo json_encode([
-            'success' => 'Заметка успешно созданна',
-            'data'=> $data,
-    ]);
 
-    die;
     $id = $db->query(
         'INSERT INTO notes (`text`, `color`, `user_id`) VALUES (?,?,?)',
         $values)->getInsertId();
-    if($id){
-//        echo json_encode([
-//                'success' => 'Заметка успешно созданна',
-//                'data'=> $data]
-//        );
-    }else{
+    if ($id) {
+        $data['id'] = $id;
+        echo json_encode([
+                'success' => 'Заметка успешно созданна',
+                'data' => $data]
+        );
+    } else {
         echo json_encode(['error' => 'Упс! Что то пошло не так, произошла ошибка!']);
     }
-        die;
-    }
-else{
+    die;
+
+} else {
     $errors = [];
-    foreach($validator->getErrors() as $key => $values ){
+    foreach ($validator->getErrors() as $key => $values) {
         foreach ($values as $error) {
-            $errors[] =  $error;
+            $errors[] = $error;
         }
     };
 
-    echo json_encode(['error'=>$errors]);
+    echo json_encode(['error' => $errors]);
 }
 
 die;
