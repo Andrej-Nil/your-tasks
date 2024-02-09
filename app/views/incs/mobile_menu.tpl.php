3c09181e57d3ce5cb6d7185e92b9a0dc343beb87
Andrej-Nil <and.kucheroff@yandex.ru>
2024-02-09 17:09:52 +0500

    edit

--- app/views/incs/mobile_menu.tpl.php
+++ app/views/incs/mobile_menu.tpl.php
@@ -1,5 +1,25 @@
 <div class="mobile-menu">
     <div class="mobile-menu__inner">
-        <div class="mobile-menu__list"></div>
+        <div class="mobile-menu__list">
+
+            <a href="/" class="mobile-menu__item">
+                <img src="<?= IMG . '/icons/home.svg' ?>" alt="" class="mobile-menu__icon">
+            </a>
+
+            <a href="/profile" class="mobile-menu__item">
+                <img src="<?= IMG . '/icons/user.svg' ?>" alt="" class="mobile-menu__icon">
+            </a>
+
+            <a href="/tasks/create" class="mobile-menu__item">
+                <img src="<?= IMG . '/icons/plus.svg' ?>" alt="" class="mobile-menu__icon">
+            </a>
+
+            <a href="/tasks/index" class="mobile-menu__item">
+                <img src="<?= IMG . '/icons/list.svg' ?>" alt="" class="mobile-menu__icon">
+            </a>
+            <span class="mobile-menu__item">
+                <img src="<?= IMG . '/icons/short.svg' ?>" alt="" class="mobile-menu__icon">
+            </span>
+        </div>
     </div>
 </div>
